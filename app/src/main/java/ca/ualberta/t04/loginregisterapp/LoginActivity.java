21b46565ce7d34156cebe2c800c938154095e90a
Unknown <jma4@ualberta.ca>
2019-02-04 21:17:40 -0700

    Update Login Activity

---
+++ app/src/main/java/ca/ualberta/t04/loginregisterapp/LoginActivity.java
@@ -0,0 +1,67 @@
+package ca.ualberta.t04.loginregisterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+
+import cz.msebera.android.httpclient.Header;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userNameEditText, passwordEditText;
+    String username, password;
+
+    AsyncHttpClient client;
+    RequestParams params;
+
+    String MYURL = "http://192.168.191.2:8080/MyFirstApp/Login";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userNameEditText = findViewById(R.id.login_username);
+        passwordEditText = findViewById(R.id.login_password);
+
+        final Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = userNameEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+
+                params = new RequestParams();
+                params.put("username", username);
+                params.put("password", password);
+                client = new AsyncHttpClient();
+                client.post(MYURL, params, new JsonHttpResponseHandler(){
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        //super.onFailure(statusCode, headers, responseString, throwable);
+                        Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                        //super.onSuccess(statusCode, headers, response);
+                        Log.d("mjy", response.toString());
+                        Toast.makeText(LoginActivity.this, "Login Success " + response, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/ca/ualberta/t04/loginregisterapp/MainActivity.java
+++ app/src/main/java/ca/ualberta/t04/loginregisterapp/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.ualberta.t04.loginregisterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +14,7 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -47,14 +50,18 @@ public class MainActivity extends AppCompatActivity {
                 client = new AsyncHttpClient();
                 client.post(MYURL,params, new JsonHttpResponseHandler(){
                     @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                         super.onSuccess(statusCode, headers, response);
+                        Log.d("mjy", response.toString());
                         Toast.makeText(MainActivity.this, "Submit success" + response, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
                     }
 
                     @Override
                     public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                        super.onFailure(statusCode, headers, responseString, throwable);
+                        //super.onFailure(statusCode, headers, responseString, throwable);
+                        Log.d("mjy", throwable.getMessage());
                         Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
                     }
                 });
